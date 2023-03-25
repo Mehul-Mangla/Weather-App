c5de546cb20f6a06e0dba0498382c1d88168a6dd
Mehul Mangla <mangla.mehul@yahoo.in>
2023-03-25 18:56:20 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+
+
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    // const APIKey = 'd3f805d080ccb22e2f4a68907a57c5bc';
+    const APIKey = config.API_KEY;
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humiditySpan = document.querySelector('.weather-details .humidity span');
+            const windSpan = document.querySelector('.weather-details .wind span');
+
+            switch (data.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            const temp = Math.floor(data.main.temp);
+            const weatherDescription = data.weather[0].description;
+            const humidity = data.main.humidity;
+            const windSpeed = Math.floor(data.wind.speed);
+
+            temperature.innerHTML = `${temp}<span>°C</span>`;
+            description.innerHTML = `${weatherDescription}`;
+            humiditySpan.innerHTML = `${humidity}%`;
+            windSpan.innerHTML = `${windSpeed}   Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+        });
+
+});
